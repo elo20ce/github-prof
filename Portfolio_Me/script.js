4f93432f99eb6087eda55ccdeacc0db4995bb80f
Mawulolo TONA <tonamawulolo@gmail.com>
2023-07-08 16:40:42 +0200

    Enfin le Portfolio exemplaire à bout

--- Portfolio_Me/script.js
+++ Portfolio_Me/script.js
@@ -1,9 +1,48 @@
 // basculer la barre de navigation de l'icône
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // section de défilement
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // liens de barre de navigation actifs
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animation on scroll 
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
     //  En-tête collant
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // supprimer l'icône de basculement et la barre de navigation lorsque vous cliquez sur les liens de la barre de navigation (défilement)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
